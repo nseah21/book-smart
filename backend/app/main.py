0f279743aab025b6fd08b6cb3a6466a56bc18f18
nseah21 <nseah21@gmail.com>
2025-01-17 00:53:30 +0800

    config: update cors origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI
-from app.routers import meetings, participants, recurrences, tasks, categories, reminders, notifications, summarizer, signup, login
+from app.routers import (
+    meetings,
+    participants,
+    recurrences,
+    tasks,
+    categories,
+    reminders,
+    notifications,
+    summarizer,
+    signup,
+    login,
+)
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=[
+        "https://booksmart.nseah.dev",
+        "https://www.booksmart.nseah.dev",
+        "https://whale-app-cehwa.ondigitalocean.app",
+        "https://www.whale-app-cehwa.ondigitalocean.app",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 async def root():
@@ -19,11 +36,15 @@ async def root():
 
 app.include_router(meetings.router, prefix="/meetings", tags=["Meetings"])
 app.include_router(participants.router, prefix="/participants", tags=["Participants"])
-app.include_router(recurrences.router, prefix="/recurrences", tags=["Recurring Meetings"])
+app.include_router(
+    recurrences.router, prefix="/recurrences", tags=["Recurring Meetings"]
+)
 app.include_router(tasks.router, prefix="/tasks", tags=["Task"])
 app.include_router(categories.router, prefix="/categories", tags=["Categories"])
 app.include_router(reminders.router, prefix="/reminders", tags=["Reminders"])
-app.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
+app.include_router(
+    notifications.router, prefix="/notifications", tags=["Notifications"]
+)
 app.include_router(summarizer.router, prefix="/summarizer", tags=["Summarizer"])
 app.include_router(signup.router, prefix="/signup", tags=["Signup"])
 app.include_router(login.router, prefix="/login", tags=["Login"])
